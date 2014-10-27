65f90487711e75a9808c16968b2c80653f53b70a
lbvienna <lenny.bronner@gmail.com>
2014-10-26 21:47:36 -0700

    learning how to use git

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,9 @@
 import tweepy, sys, codecs, os, csv, codecs
 
 class Tokenizer:
-	def __init__(self):
+    def __init__(self):
+        pass
 
 class ReadCSV:
-	def __init__(self):
+    def __init__(self):
+        pass
