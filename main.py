c2ea798830f05c082f694749bb7348a8b2af95e6
Anton Apostolatos <antonaf@DN0a22403e.SUNet>
2014-11-12 01:38:32 -0800

    Fixed multithreading

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from communicator import *
 # from tokenizer.py import Tokenizer
 
 if __name__ == "__main__":
-    #fileName = 'sample.csv'
     fileName = 'users_' + time.strftime("%H:%M:%S") + '.json'
-    # TweetStreamingCommunicator(Terms = ["the"], Locations = None, Filename = fileName)
-    # TweetCommunicator()
     userCommunicator = UserCommunicator(fileName)
     users = userCommunicator.get_users(max_users=100)
     userCommunicator.fill_users(users)
